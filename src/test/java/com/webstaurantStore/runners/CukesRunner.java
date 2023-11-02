4c2cc7c5b5d843c1a12db70c14b1d5bac8097fe7
cyykablyat <batuhangunn96@gmail.com>
2023-11-02 19:33:23 -0400

    Added Runners,Driver class and Config reader.

---
+++ src/test/java/com/webstaurantStore/runners/CukesRunner.java
@@ -0,0 +1,21 @@
+package com.webstaurantStore.runners;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/webstaurantStore/step_Definitions",
+        dryRun = false,
+        tags = "",
+        publish = true
+)
+public class CukesRunner {
+}
